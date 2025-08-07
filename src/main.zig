bd1e564a87ac57dc30922dac9a5a0e566a270985
jeffersonfparil <jeffersonparil@gmail.com>
2025-08-07 11:13:09 +1000

    testing creds

--- src/main.zig
+++ src/main.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 const mlp_pcve = @import("mlp_pcve");
 
 pub fn main() !void {
-    // Prints to stderr, ignoring potential errors.
+    
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
     try mlp_pcve.bufferedPrint();
 }
